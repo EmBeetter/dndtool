b6f494a69e182202f94094fe6a9dae9df6a73ac7
EmBeetter <EmBeetter@users.noreply.github.com>
2024-06-02 17:53:29 +0000

    deploy: c4aed1bf4996b9807e6896a218ad5c8f09b5aa5b

--- js/notifications.js
+++ js/notifications.js
@@ -2,3 +2,17 @@
     var audio = new Audio('audio/notification.mp3');
     audio.play();
 };
+function requestNotificationPermission() {
+    if (Notification.permission !== 'granted') {
+        Notification.requestPermission();
+    }
+}
+
+function showNotification(notification) {
+    if (Notification.permission === 'granted') {
+        new Notification(notification.title, {
+            body: notification.message,
+            icon: notification.icon
+        });
+    }
+}
